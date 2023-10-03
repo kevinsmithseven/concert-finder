3ebe92c0608ff7badf4238273e5ad85385757ebb
CRich96 <you@example.comchris575502@icloud.com>
2023-10-03 14:04:37 -0500

    Fetch Api Function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Global variables
 // var concertAPIKey = 
-// var weatherAPIKey =
+var weatherAPIKey = "4a5b27e8dacd4394811170611230310"
 var header = document.getElementById("header");
 var savedDropDown = document.getElementById("selector");
 var hero = document.querySelector(".hero");
@@ -14,3 +14,43 @@ var inputCity = document.getElementById("city");
 var inputArtist = document.getElementById("artist")
 var submitButton = document.getElementById("submit-btn");
 
+// const url = "https://api.weatherapi.com/v1/forecast.json" + weatherAPIKey;
+   
+//         function getWeatherData() {
+//             fetch(url)
+//                 // .then(handleErrors)
+//                 .then(function (response) {
+//                     return response.json();
+//                 })
+//                 .then(function (data) {
+//                     console.log(data);
+//                 })
+
+function getWeatherData(){
+    var location = 'Dallas'; // Replace with your desired location
+    const apiUrl = `https://api.weatherapi.com/v1/forecast.json?q=${location}&key=${weatherAPIKey}`;
+    
+    fetch(apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Handle the weather data here
+        console.log(data);
+    
+        // Extract specific weather information from the 'data' object as needed
+        const temperature = data.current.temp_f;
+        console.log(temperature);
+        const weatherDescription = data.current.condition[0];
+        console.log(weatherDescription);
+        // ... and so on
+      })
+      .catch(error => {
+        // Handle errors here
+        console.error('Fetch error:', error);
+      });
+}
+    getWeatherData()
